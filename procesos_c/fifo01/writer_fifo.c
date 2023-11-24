105b2b4b05fc303c3dcbb898a49b26a22ed60656
pablo <pablopallas00@gmail.com>
2023-11-24 21:23:39 +0100

    reorganizo fifos

--- procesos_c/fifo01/writer_fifo.c
+++ procesos_c/fifo01/writer_fifo.c
@@ -10,6 +10,7 @@ int main(){
     //el primer argumento que le pasamos es una ruta donde situar el fifo
     //el segundo argumento que le pasamos es el modo o permisos que queremos darle
     mkfifo("/tmp/mi_fifo",0666); //permisos escritura y lectura, 0 para formato octal
+    // mkfifo() devuelve un entero, si es -1 ha ocurrido un error
 
     //open nos devuelve un descriptor
     fd = open("/tmp/mi_fifo", O_WRONLY); //lo abrimos en modo escritura
